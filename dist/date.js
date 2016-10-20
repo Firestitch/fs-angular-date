72cced7834ef64a9b4b9e4e177965e7bc68a38ea
Raymond <ray@firestitch.com>
2016-10-20 13:14:48 -0400

    commit

--- dist/date.js
+++ dist/date.js
@@ -16,4 +16,31 @@
         };
     });
 })();
+(function () {
+    'use strict';
+
+    angular.module('fs-angular-date')
+    .factory('fsDate', function() {
+
+
+
+        return {
+
+
+        };
+
+
+        function ago(date) {
+
+        }
+
+        function format(date) {
+
+        }
+
+        function range(from,to) {
+
+        }
+    });
+})();
 
---
+++ src/app/scripts/services/service.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    angular.module('fs-angular-date')
+    .factory('fsDate', function() {
+
+
+
+        return {
+
+
+        };
+
+
+        function ago(date) {
+
+        }
+
+        function format(date) {
+
+        }
+
+        function range(from,to) {
+
+        }
+    });
+})();
